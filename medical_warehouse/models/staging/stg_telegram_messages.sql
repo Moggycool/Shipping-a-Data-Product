0b501eb1737d3c12674df167bf35c77e7bd02359
Moggycool <mogycool@gmail.com>
2026-01-16 18:22:57 +0300

    Add dbt models

---
+++ medical_warehouse/models/staging/stg_telegram_messages.sql
@@ -0,0 +1,22 @@
+with src as (
+  select
+    channel_username,
+    message_id,
+    channel_title,
+    message_ts,
+    message_text,
+    views,
+    forwards,
+    reply_count,
+    has_media,
+    has_image,
+    media_type,
+    media_path,
+    partition_date,
+    source_file,
+    ingested_at,
+    payload
+  from raw.telegram_messages
+)
+
+select * from src
